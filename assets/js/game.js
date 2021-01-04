bbcf968b5faeb160af4831dd4843e96bc1284a78
LinusLoof <linus.loof96@gmail.com>
2021-01-04 18:04:55 +0100

    ms2 project start

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+let sequence = [];
+
+let humanSequence = [];
+
+const startButton = document.querySelector('.js-start');
+const info = document.querySelector('.js-info');
+
+function playGame() {
+  startButton.classList.add('hidden');
+  info.classList.remove('hidden');
+  info.textContent = 'Wait for the computer';
+}
+
+startButton.addEventListener('click', startGame);
